176ed4c475c66eb2c8f1e5a9f24702e08fba4e1d
fredrickmugish <fredrickgotfried@gmail.com>
2024-03-20 21:19:47 +0000

    first commit

--- website/views.py
+++ website/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+          form = UserCreationForm(request.POST)
+          if form.is_valid():
+               form.save()
+               
+    context = {'form':form}
+    return render(request, 'register.html', context)
